08e018d67bef8d1922bccefd48b0d8072855b8ad
Bole <jelenabole@gmail.com>
2021-05-18 10:31:17 +0200

    docker db init fixup

--- docker/init.sql
+++ docker/init.sql
@@ -1,10 +1,6 @@
-create sequence hibernate_sequence;
-
-alter sequence hibernate_sequence owner to postgres;
-
-create table if not exists customer
+create table customer
 (
-	id bigint not null
+	id bigserial not null
 		constraint customer_pkey
 			primary key,
 	email varchar(255),
@@ -14,9 +10,9 @@ create table if not exists customer
 
 alter table customer owner to postgres;
 
-create table if not exists product
+create table product
 (
-	id bigint not null
+	id bigserial not null
 		constraint product_pkey
 			primary key,
 	code varchar(10) not null
@@ -25,35 +21,40 @@ create table if not exists product
 	description varchar(255),
 	is_available boolean not null,
 	name varchar(255),
-	price_hrk real not null
+	price_hrk numeric(19,2) not null
 );
 
 alter table product owner to postgres;
 
-create table if not exists webshop_order
+create table webshop_order
 (
-	id bigint not null
+	id bigserial not null
 		constraint webshop_order_pkey
 			primary key,
 	status varchar(255) not null,
-	total_price_eur real not null,
-	total_price_hrk real not null
+	total_price_eur numeric(19,2) default 0 not null,
+	total_price_hrk numeric(19,2) default 0 not null,
+	customer_id bigint
+		constraint fkfwgyft2j50y1oyor9y5455djq
+			references customer
 );
 
 alter table webshop_order owner to postgres;
 
-create table if not exists order_item
+create table order_item
 (
-	order_id bigint not null
-		constraint fkdk3kexcjjiok4a2p3v0iidowk
-			references webshop_order,
-	product_id bigint
+	id bigserial not null
+		constraint order_item_pkey
+			primary key,
+	quantity integer not null
+		constraint order_item_quantity_check
+			check (quantity >= 0),
+	product_id bigint not null
 		constraint fk551losx9j75ss5d6bfsqvijna
 			references product,
-	quantity integer,
-	index_id integer not null,
-	constraint order_item_pkey
-		primary key (order_id, index_id)
+	order_id bigint
+		constraint fkdk3kexcjjiok4a2p3v0iidowk
+			references webshop_order
 );
 
 alter table order_item owner to postgres;
